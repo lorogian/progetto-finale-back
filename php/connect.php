d27c27fa62b52293d9a8f6cfc27e86d7b9391211
lai2-01 <lai2-01@itismeucci.com>
2023-05-23 10:42:02 +0200

    primo

---
+++ php/connect.php
@@ -0,0 +1,10 @@
+<?php
+$username="root";
+$password="";
+$database="ListinoMoto";
+$host="localhost";
+
+$sqli=new mysqli($host,$username,$password,$database)
+or die("nabbo connessione fallita");
+
+?>
---
+++ php/index.php
@@ -0,0 +1,18 @@
+
+<?php
+ include './connect.php';
+ function InvioDB(){
+    
+ }
+ if($_SERVER['SERVER_NAME']=="")
+ {
+    if($_SERVER["REQUEST_METHOD"]=="POST")
+    else if($_SERVER["REQUEST_METHOD"]=="GET")
+    else if($_SERVER["REQUEST_METHOD"]=="DELETE")
+    else if($_SERVER["REQUEST_METHOD"]=="HEAD")
+ }
+
+
+
+
+?>
