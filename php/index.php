b50fe62a34d1cf0a8d72f0b906836038c0e8a97d
lai2-01 <lai2-01@itismeucci.com>
2023-05-26 08:51:32 +0200

    si

--- php/index.php
+++ php/index.php
@@ -8,6 +8,14 @@
  {
     if($_SERVER["REQUEST_METHOD"]=="POST")
     else if($_SERVER["REQUEST_METHOD"]=="GET")
+    {
+      $myquery=query("select * from moto");
+      $risulato=$sqli->$myquery;
+      while($row=$risulato->fetch_assoc()){
+         $json[]=$row;
+      }
+
+    }
     else if($_SERVER["REQUEST_METHOD"]=="DELETE")
     else if($_SERVER["REQUEST_METHOD"]=="HEAD")
  }
